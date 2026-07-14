146bed540b8284e64214d7d77bec90611fd4faba
shalomadepoju80-png <shalomadepoju80@gmail.com>
2026-07-14 00:08:42 -0500

    Add supportsPrefixExecution property to bug-report command

--- src/commands/Utility/bug-report.js
+++ src/commands/Utility/bug-report.js
@@ -10,7 +10,7 @@ export default {
   data: new SlashCommandBuilder()
     .setName("bug-report")
     .setDescription("Report a bug with ClanBot"),
-
+supportsPrefixExecution: false,
   async execute(interaction) {
 
     if (!interaction.isChatInputCommand()) return;
