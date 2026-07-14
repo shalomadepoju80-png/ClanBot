4779a3e81c2ff9fdf8ad545820d53cc3956d8de4
shalomadepoju80-png <shalomadepoju80@gmail.com>
2026-07-14 00:30:40 -0500

    Update bug-report.js

--- src/commands/Utility/bug-report.js
+++ src/commands/Utility/bug-report.js
@@ -13,7 +13,7 @@ export default {
   
     .setName("bug-report")
     .setDescription("Report a bug with ClanBot"),
-supportsPrefixExecution: true,
+supportsPrefixExecution: false,
   async execute(interaction) {
 
   if (!interaction.isChatInputCommand()) return;
