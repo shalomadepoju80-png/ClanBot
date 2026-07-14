3d510b024700a1997d1ee421d7e3f0b9a1124c67
shalomadepoju80-png <shalomadepoju80@gmail.com>
2026-07-14 00:41:12 -0500

    Create bugreport.js

---
+++ src/commands/Community/bugreport.js
@@ -0,0 +1,55 @@
+import {
+  SlashCommandBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  ActionRowBuilder
+} from "discord.js";
+
+export default {
+  prefix: false,
+
+  data: new SlashCommandBuilder()
+    .setName("bugreport")
+    .setDescription("Report a bug"),
+
+  async execute(interaction) {
+
+    const modal = new ModalBuilder()
+      .setCustomId("bug_report_modal")
+      .setTitle("🐛 Bug Report");
+
+    const title = new TextInputBuilder()
+      .setCustomId("bug_title")
+      .setLabel("Bug Title")
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
+
+    const description = new TextInputBuilder()
+      .setCustomId("bug_description")
+      .setLabel("Describe the bug")
+      .setStyle(TextInputStyle.Paragraph)
+      .setRequired(true);
+
+    const steps = new TextInputBuilder()
+      .setCustomId("bug_steps")
+      .setLabel("Steps to reproduce")
+      .setStyle(TextInputStyle.Paragraph)
+      .setRequired(true);
+
+    const proof = new TextInputBuilder()
+      .setCustomId("bug_proof")
+      .setLabel("Proof (optional)")
+      .setStyle(TextInputStyle.Paragraph)
+      .setRequired(false);
+
+    modal.addComponents(
+      new ActionRowBuilder().addComponents(title),
+      new ActionRowBuilder().addComponents(description),
+      new ActionRowBuilder().addComponents(steps),
+      new ActionRowBuilder().addComponents(proof)
+    );
+
+    await interaction.showModal(modal);
+  }
+};
