863d744f0080609ccf98f2d64dcd59f5e3715d02
shalomadepoju80-png <shalomadepoju80@gmail.com>
2026-07-13 23:46:46 -0500

    Refactor bug report modal by removing inputs
    Removed input fields for bug report modal.

--- src/commands/Utility/bug-report.js
+++ src/commands/Utility/bug-report.js
@@ -17,36 +17,7 @@ export default {
       .setCustomId("bug_report_modal")
       .setTitle("🐛 Bug Report");
 
-    const title = new TextInputBuilder()
-      .setCustomId("bug_title")
-      .setLabel("Bug Title")
-      .setStyle(TextInputStyle.Short)
-      .setRequired(true);
-
-    const description = new TextInputBuilder()
-      .setCustomId("bug_description")
-      .setLabel("Describe the bug")
-      .setStyle(TextInputStyle.Paragraph)
-      .setRequired(true);
-
-    const steps = new TextInputBuilder()
-      .setCustomId("bug_steps")
-      .setLabel("Steps to reproduce")
-      .setStyle(TextInputStyle.Paragraph)
-      .setRequired(true);
-
-    const proof = new TextInputBuilder()
-      .setCustomId("bug_proof")
-      .setLabel("Proof (image/link)")
-      .setStyle(TextInputStyle.Short)
-      .setRequired(false);
-
-    modal.addComponents(
-      new ActionRowBuilder().addComponents(title),
-      new ActionRowBuilder().addComponents(description),
-      new ActionRowBuilder().addComponents(steps),
-      new ActionRowBuilder().addComponents(proof)
-    );
+    // your inputs here...
 
     await interaction.showModal(modal);
   }
