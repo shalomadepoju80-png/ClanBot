dbc057bc9b4c48ac50aca56ce17d56cee88c1016
shalomadepoju80-png <shalomadepoju80@gmail.com>
2026-07-24 14:07:41 -0500

    Update eat command to check Bug Points without mention
    Refactor 'execute' method to remove user mention requirement and add Bug Points check.

--- src/prefixCommands/eat.js
+++ src/prefixCommands/eat.js
@@ -1,13 +1,29 @@
+import fs from "fs";
+
+const DATA_FILE = "./src/data/bugpoints.json";
+
 export default {
   name: "eat",
 
-  async execute(message, args) {
-    const user = message.mentions.users.first();
+  async execute(message) {
+    let data = {};
 
-    if (!user) {
-      return message.reply("Mention someone to eat.");
+    if (fs.existsSync(DATA_FILE)) {
+      data = JSON.parse(fs.readFileSync(DATA_FILE, "utf8"));
     }
 
-    await message.reply(`🍽️ ${message.author} ate ${user}!`);
+    const points = data[message.author.id] || 0;
+
+    if (points < 4) {
+      return message.reply("❌ You need 4 Bug Points to use this!");
+    }
+
+    data[message.author.id] = points - 4;
+
+    fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+
+    const user = message.mentions.users.first() || message.author;
+
+    message.reply(`🍽️ ${message.author} ate ${user}! (-4 🐛 Bug Points)`);
   }
 };
