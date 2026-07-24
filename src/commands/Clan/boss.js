7cdd0aaadf1dc30c811eb42fae32d0628644120d
shalomadepoju80-png <shalomadepoju80@gmail.com>
2026-07-24 09:38:01 -0500

    Update OWNER_ID in boss.js

--- src/commands/Clan/boss.js
+++ src/commands/Clan/boss.js
@@ -1,7 +1,7 @@
 import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
 import fs from "fs";
 import { addClanPoints } from "../../services/clanPoints.js";
-const OWNER_ID = "1368313910943547413";
+const OWNER_ID = "1300196895167217777";
 
 const bossFile = "./src/data/boss.json";
 
