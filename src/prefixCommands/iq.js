48879827d15ac23185b49692c7a05ca8de5b0e70
shalomadepoju80-png <shalomadepoju80@gmail.com>
2026-07-24 14:05:28 -0500

    Implement Bug Point deduction for IQ command
    Added functionality to check and deduct Bug Points before replying with IQ.

--- src/prefixCommands/iq.js
+++ src/prefixCommands/iq.js
@@ -1,7 +1,34 @@
+import fs from "fs";
+
+const DATA_FILE = "./src/data/bugpoints.json";
+
 export default {
   name: "iq",
 
   async execute(message) {
-    await message.reply(`🧠 ${message.author} has 100 IQ!`);
+    let data = {};
+
+    if (fs.existsSync(DATA_FILE)) {
+      data = JSON.parse(fs.readFileSync(DATA_FILE, "utf8"));
+    }
+
+    const points = data[message.author.id] || 0;
+
+    if (points < 1) {
+      return message.reply("❌ You need 1 Bug Point to use this!");
+    }
+
+    data[message.author.id] = points - 1;
+
+    fs.writeFileSync(
+      DATA_FILE,
+      JSON.stringify(data, null, 2)
+    );
+
+    const iq = Math.floor(Math.random() * 100) + 1;
+
+    await message.reply(
+      `🧠 ${message.author} has **${iq} IQ**! (-1 🐛 Bug Point)`
+    );
   }
 };
