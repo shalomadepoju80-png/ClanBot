8855439df2d2ee39d21b9431437e6aea2e93242c
shalomadepoju80-png <shalomadepoju80@gmail.com>
2026-07-24 14:08:04 -0500

    Refactor cat-eat command to include Bug Points check
    Updated the cat-eat command to read from a data file and check Bug Points before executing the action.

--- src/prefixCommands/cat-eat.js
+++ src/prefixCommands/cat-eat.js
@@ -1,13 +1,29 @@
+import fs from "fs";
+
+const DATA_FILE = "./src/data/bugpoints.json";
+
 export default {
   name: "cat-eat",
 
-  async execute(message, args) {
-    const user = message.mentions.users.first();
+  async execute(message) {
+    let data = {};
 
-    if (!user) {
-      return message.reply("Mention someone.");
+    if (fs.existsSync(DATA_FILE)) {
+      data = JSON.parse(fs.readFileSync(DATA_FILE, "utf8"));
     }
 
-    await message.reply(`🐱 Cat ate ${user}! Mmm yummy!`);
+    const points = data[message.author.id] || 0;
+
+    if (points < 5) {
+      return message.reply("❌ You need 5 Bug Points to use this!");
+    }
+
+    data[message.author.id] = points - 5;
+
+    fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+
+    const user = message.mentions.users.first() || message.author;
+
+    message.reply(`🐱🍴 Cat ate ${user}! (-5 🐛 Bug Points)`);
   }
 };
