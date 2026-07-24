a07bad85771c922ebe07462874577e763a050aba
shalomadepoju80-png <shalomadepoju80@gmail.com>
2026-07-24 14:16:51 -0500

    Create Iq.js

---
+++ src/commands/Owner/Iq.js
@@ -0,0 +1,38 @@
+import { SlashCommandBuilder } from "discord.js";
+import fs from "fs";
+
+const DATA_FILE = "./src/data/bugpoints.json";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("iq")
+    .setDescription("Spend 1 Bug Point for IQ"),
+
+  async execute(interaction) {
+    let data = {};
+
+    if (fs.existsSync(DATA_FILE)) {
+      data = JSON.parse(fs.readFileSync(DATA_FILE, "utf8"));
+    }
+
+    const points = data[interaction.user.id] || 0;
+
+    if (points < 1) {
+      return interaction.reply({
+        content: "❌ You need 1 Bug Point.",
+        ephemeral: true
+      });
+    }
+
+    data[interaction.user.id] -= 1;
+
+    fs.writeFileSync(
+      DATA_FILE,
+      JSON.stringify(data, null, 2)
+    );
+
+    await interaction.reply(
+      `🧠 ${interaction.user} gained IQ! (-1 🐛 Bug Point)`
+    );
+  }
+};
